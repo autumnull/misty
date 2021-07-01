a3158f10f882ead126bf9be51740bb6fd3d2f4b6
Autumn! <autumnull@protonmail.com>
2021-07-01 17:01:29 +0100

    completed tutorial

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -1,40 +1,42 @@
-/*
-  ==============================================================================
-
-    This file contains the basic framework code for a JUCE plugin editor.
-
-  ==============================================================================
-*/
-
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
 
-//==============================================================================
-MistyAudioProcessorEditor::MistyAudioProcessorEditor (MistyAudioProcessor& p)
-    : AudioProcessorEditor (&p), audioProcessor (p)
+MistyAudioProcessorEditor::MistyAudioProcessorEditor(MistyAudioProcessor& p) :
+	AudioProcessorEditor(&p),
+	audioProcessor(p)
 {
-    // Make sure that before the constructor has finished, you've set the
-    // editor's size to whatever you need it to be.
-    setSize (400, 300);
+	midiVolume.setSliderStyle(juce::Slider::LinearBarVertical);
+	midiVolume.setRange(0.0, 127.0, 1.0);
+	midiVolume.setTextBoxStyle(juce::Slider::NoTextBox, false, 90, 0);
+	midiVolume.setPopupDisplayEnabled(true, false, this);
+	midiVolume.setTextValueSuffix(" Volume");
+	midiVolume.setValue(1.0);
+	addAndMakeVisible(&midiVolume);
+	midiVolume.addListener(this);
+
+    setSize(200, 200);
 }
 
 MistyAudioProcessorEditor::~MistyAudioProcessorEditor()
 {
+
 }
 
-//==============================================================================
-void MistyAudioProcessorEditor::paint (juce::Graphics& g)
+void MistyAudioProcessorEditor::paint(juce::Graphics& g)
 {
-    // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+    g.fillAll(juce::Colours::white);
 
-    g.setColour (juce::Colours::white);
-    g.setFont (15.0f);
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+    g.setColour(juce::Colours::black);
+    g.setFont(15.0f);
+    g.drawFittedText("Midi Volume", 0, 0, getWidth(), 30, juce::Justification::centred, 1);
 }
 
 void MistyAudioProcessorEditor::resized()
 {
-    // This is generally where you'll want to lay out the positions of any
-    // subcomponents in your editor..
+	midiVolume.setBounds(40, 30, 20, getHeight()-60);
+}
+
+void MistyAudioProcessorEditor::sliderValueChanged(juce::Slider* slider)
+{
+	audioProcessor.noteOnVel = midiVolume.getValue();
 }
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -1,33 +1,25 @@
-/*
-  ==============================================================================
-
-    This file contains the basic framework code for a JUCE plugin editor.
-
-  ==============================================================================
-*/
-
 #pragma once
 
 #include <JuceHeader.h>
 #include "PluginProcessor.h"
 
-//==============================================================================
-/**
-*/
-class MistyAudioProcessorEditor  : public juce::AudioProcessorEditor
+class MistyAudioProcessorEditor :
+	public juce::AudioProcessorEditor,
+	private juce::Slider::Listener
 {
 public:
-    MistyAudioProcessorEditor (MistyAudioProcessor&);
+    MistyAudioProcessorEditor(MistyAudioProcessor&);
     ~MistyAudioProcessorEditor() override;
 
-    //==============================================================================
-    void paint (juce::Graphics&) override;
+    void paint(juce::Graphics&) override;
     void resized() override;
 
 private:
-    // This reference is provided as a quick way for your editor to
-    // access the processor object that created it.
+	void sliderValueChanged(juce::Slider* slider) override;
+
     MistyAudioProcessor& audioProcessor;
 
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MistyAudioProcessorEditor)
+    juce::Slider midiVolume;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MistyAudioProcessorEditor)
 };
